205d586e5444a0152bbe89c4039288be02681cba
HassanZakeri <HassanZakeri1381@gmail.com>
2024-05-15 11:13:06 +0330

    Add project files.

---
+++ Layer.Domain.Migrator/Migrator/2024-15-05-10-00-createTbl_Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer.Domain.Migrator.Migrator
+{
+    public class _2024_15_05_10_00_createTbl_Users
+    {
+    }
+}
---
+++ Layer.Domian.Entities/DB/DbAplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer.Domian.Entities.DB
+{
+    public class DbAplicationContext
+    {
+        public static string connectionString = "Server=.;Initial Catalog=DB_ChatRoom1;Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=true;"; 
+    }
+}
